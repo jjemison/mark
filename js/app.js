0b855c9349df3a414722796ee5ff65f0475e0ba3
Jordan <jordanjemison@gmail.com>
2015-05-19 09:43:19 -0600

    create profile view

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ app.config(function($routeProvider){
       templateUrl: 'views/signup.html',
       controller: 'signupCtrl'
     })
+  .when('/profile', {
+      templateUrl: 'views/profile.html',
+      controller: 'profileCtrl'
+    })
   .otherwise({
       redirectTo: '/signup'
     });
---
+++ js/controllers/profileCtrl.js
@@ -0,0 +1,11 @@
+var app = angular.module('mark');
+
+app.controller('profileCtrl', function($scope){
+  
+
+
+
+
+
+  
+});
