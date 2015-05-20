16a80b2f576d47946b16b67db9c7ca9271d08368
Jordan <jordanjemison@gmail.com>
2015-05-20 14:36:43 -0600

    add carousel

--- js/controllers/signupCtrl.js
+++ js/controllers/signupCtrl.js
@@ -1,6 +1,15 @@
 var app = angular.module('mark');
 
-app.controller('signupCtrl', function($scope, $modal, $log){
-  
-  
+app.controller('signupCtrl', function($scope){
+  $scope.myInterval = 3000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 325 + slides.length + 1;
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/400',
+    });
+  };
+  for (var i=0; i<3; i++) {
+    $scope.addSlide();
+  } 
 });
